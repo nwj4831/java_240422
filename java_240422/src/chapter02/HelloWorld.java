88cdea3bf1ee359bd56a9a295eb6fdb003d23cb9
nwj4831 <nwj4831@naver.com>
2024-04-23 17:44:09 +0900

    20240423 배운내용

---
+++ java_240422/src/chapter02/HelloWorld.java
@@ -0,0 +1,20 @@
+package chapter02;
+
+/*
+ * 작성일 : 2024-04-23
+ * 작성자 : 홍길동 
+ * 내용 : Helloworld 클래스로 문자열 출력
+*/
+
+public class HelloWorld {
+	public static void main(String[] args) {
+//		string : 문자열 타입
+		//message : 변수(명)
+//		String message; //변수 선언
+//		String message ="헬로 월드!";//변수값 할당(변수 초기화)
+		String message ="헬로 월드!";
+		System.out.println(message);
+		message="웰컴 투헬!";
+		System.out.println(message);
+	}
+}
