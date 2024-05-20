77a887ddc87810b7d51043cf1fc692a5b2d96c78
nwj4831 <nwj4831@naver.com>
2024-05-20 17:45:35 +0900

    052420

--- java_240422/src/question07/ArrayListEx.java
+++ java_240422/src/question07/ArrayListEx.java
@@ -1,24 +1,33 @@
 package question07;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class ArrayListEx {
 	public static void main(String[] args) {
 		Scanner scanner=new Scanner(System.in);
-		System.out.println("이름을 입력하세요>>");
+		ArrayList<String> a = new ArrayList<>();
 		
-		List<String> list = new ArrayList<>();
-		list.add(scanner.next());
-		list.add(scanner.next());
-		list.add(scanner.next());
-		list.add(scanner.next());
-		
-		for (int i = 0; i < list.size(); i++) {
-			String str = list.get(i);
-			System.out.print(str+" ");
-			}
-		System.out.println("가장 긴 이름은");
+		for (int i = 0; i < 4; i++) {
+			System.out.println("이름을 입력하세요>>");
+			String s =(scanner.next());
+			a.add(s);
 		}
-}
+		for (int i = 0; i < a.size(); i++) {
+			String name =a.get(i);
+			System.out.print(name+" ");
+			}
+		int longestIndex=0;
+		for (int i = 0; i < a.size(); i++) {
+//			a.get(0).length()< a.get(0).length()
+//			a.get(0).length()< a.get(1).length() = 참이면 longesIndex=i
+//			a.get(0or1).length()< a.get(2).length() = 참이면 longesIndex=i
+//			a.get(0or1or2).length()< a.get(3).length() = 참이면 longesIndex=i
+			if (a.get(longestIndex).length() <a.get(i).length()) {
+				longestIndex= i;
+			}
+		}
+		System.out.println("가장 긴 이름은"+a.get(longestIndex));
+		scanner.close();
+		}
+} 
